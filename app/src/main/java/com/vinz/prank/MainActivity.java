01e346c32da6a9654b6e9aae2ebcd23d2cde664a
logFC12 <niandrabintang@gmail.com>
2026-03-08 20:45:47 +0700

    Fix: Java typo and add gradle wrapper step

--- app/src/main/java/com/vinz/prank/MainActivity.java
+++ app/src/main/java/com/vinz/prank/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
     @Override
-    protected void Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
