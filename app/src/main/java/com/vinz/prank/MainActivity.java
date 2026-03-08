84b6ce1b22cbb16e8d026b0b4ac205d6cf931cbd
logFC12 <niandrabintang@gmail.com>
2026-03-08 21:02:52 +0700

    Full Lockdown Feature: Home, Recent, Taskbar

--- app/src/main/java/com/vinz/prank/MainActivity.java
+++ app/src/main/java/com/vinz/prank/MainActivity.java
@@ -12,11 +12,20 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // AKTIFKAN LOCKDOWN: Ngunci Home, Recent, & Taskbar
+        try {
+            startLockTask();
+        } catch (Exception e) {
+            Toast.makeText(this, "Fitur Pinning Belum Aktif!", Toast.LENGTH_SHORT).show();
+        }
+
         Button btnExit = findViewById(R.id.btnExit);
         btnExit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish(); // Ini satu-satunya cara keluar
+                // LEPAS KUNCI sebelum keluar biar HP normal lagi
+                stopLockTask();
+                finish();
             }
         });
     }
@@ -24,6 +33,6 @@ public class MainActivity extends Activity {
     // MATIIN TOMBOL BACK
     @Override
     public void onBackPressed() {
-        Toast.makeText(this, "AKSES DITOLAK! GUNAKAN TOMBOL MERAH!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "SYSTEM CRITICAL: ACCESS DENIED!", Toast.LENGTH_SHORT).show();
     }
 }
